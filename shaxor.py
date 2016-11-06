483c168f522255f8dd23635ba4709430de38c26f
Feshider <paulenkamichal@gmail.com>
2016-11-06 21:44:11 +0100

    Design and Unit Tests

--- shaxor.py
+++ shaxor.py
@@ -34,7 +34,7 @@ from argparse import ArgumentParser
 from sys import exit, stdout
 from hashlib import sha512
 from base64 import b64encode, b64decode
-from os.path import getsize
+from os import path, remove
 from getpass import getpass
 from time import time
 
@@ -49,59 +49,76 @@ class SHAXOR:
     chunk_size = 8192
 
     @staticmethod
-    def SetArgs():
-        SHAXOR.argumentParser.add_argument("-m", "--mode", help="""\"TEXTENC\" - (text) encryption,
-                                                                    \"FILE\" - (path to file) encryption/decryption,
-                                                                    \"TEXTDEC\" - (text) decryption""",
+    def set_args():
+        SHAXOR.argumentParser.add_argument("-m", "--mode", help="""\"TE\" - (text) Text Encryption mode,
+                                                                    \"F\" - (path to file) File encryption/decryption mode,
+                                                                    \"TD\" - (text) Text Decryption mode""",
                                            required=True)
-        SHAXOR.argumentParser.add_argument("-i", "--input", help="Text/File for encryption/decryption.", required=True)
+        SHAXOR.argumentParser.add_argument("-i", "--input", help="(text/file) for encryption/decryption", required=True)
         SHAXOR.argumentParser.add_argument("-o", "--output",
-                                           help="(path to file) if not set, output is printed to terminal.")
+                                           help="(path to file) if not set, output is printed to terminal")
 
     @staticmethod
-    def ParseArgs():
+    def parse_args():
         args = SHAXOR.argumentParser.parse_args()
         SHAXOR.mode = args.mode
         SHAXOR.inp = args.input
         SHAXOR.output = args.output
 
     @staticmethod
-    def GetKeys(count, mode=0):
-        keys = []
-        if mode == 0:
-            word = "encrytion/decryption"
-        elif mode == 1:
+    def show_banner():
+        banner = """\
+_____________  ____________  ________________
+__  ___/__  / / /__    |_  |/ /_  __ \__  __ \*
+_____ \__  /_/ /__  /| |_    /_  / / /_  /_/ /*
+____/ /_  __  / _  ___ |    | / /_/ /_  _, _/*
+/____/ /_/ /_/  /_/  |_/_/|_| \____/ /_/ |_|*\n"""
+        print(banner)
+        print(" DON'T USE SAME KEY TWO OR MORE TIMES!!!\n")
+
+    @staticmethod
+    def get_keys(mode=None, two=True):
+        if mode == 1:
             word = "encryption"
         elif mode == 2:
             word = "decryption"
-        for i in range(1, count+1):
-            keys.append(getpass("{0}# Enter {1} key please: ".format(i, word)))
-        if len(keys) == 2:
-            if keys[0] == keys[1]:
-                SHAXOR.key = keys[0]
+        else:
+            word = "encrytion/decryption"
+
+        key1 = getpass("1# Enter {0} key please: ".format(word))
+        if two:
+            key2 = getpass("2# Enter {0} key please: ".format(word))
+            if key1 == key2:
+                SHAXOR.key = key1
+                print()
+                return
             else:
-                print("SHAXOR.py: error: keys is different.")
+                print("shaxor.py: error: keys is different.")
                 exit()
         else:
-            SHAXOR.key = keys[0]
+            SHAXOR.key = key1
+        print()
 
     @staticmethod
-    def Decide():
-        if SHAXOR.mode == "TEXTENC":
-            SHAXOR.GetKeys(2, 1)
-            SHAXOR.EncText()
-        elif SHAXOR.mode == "TEXTDEC":
-            SHAXOR.GetKeys(1, 2)
-            SHAXOR.EncText(decrypt=True)
-        elif SHAXOR.mode == "FILE":
-            SHAXOR.GetKeys(2, 0)
-            if SHAXOR.output != None:
-                SHAXOR.EncFile()
+    def decide():
+        if SHAXOR.mode == "TE":
+            SHAXOR.get_keys(mode=1)
+            SHAXOR.enc_text()
+            return
+        elif SHAXOR.mode == "TD":
+            SHAXOR.get_keys(mode=2, two=False)
+            SHAXOR.enc_text(decrypt=True)
+            return
+        elif SHAXOR.mode == "F":
+            SHAXOR.get_keys()
+            if SHAXOR.output is not None:
+                SHAXOR.enc_file()
+                return
             else:
-                print("SHAXOR.py: error: in FILE mode is required argument -o/--output")
+                print("shaxor.py: error: in FILE mode is required argument -o/--output")
                 exit()
         else:
-            print("SHAXOR.py: error: unknown mode")
+            print("shaxor.py: error: unknown mode")
             exit()
 
     @staticmethod
@@ -112,7 +129,7 @@ class SHAXOR:
             num /= 1024.0
 
     @staticmethod
-    def UpdateProgress(pr, max):
+    def update_progress(pr, max):
         speed = int(SHAXOR.chunk_size*(1/float(time()-SHAXOR.time_temp)))
         pc = int((100 * pr) / float(max))
         stdout.write("[{0}{1}]{2}% {3}/{4}  {5}/s\r".format("#" * (pc / 4), " " * (25 - (pc / 4)), pc,
@@ -121,9 +138,9 @@ class SHAXOR:
         stdout.flush()
 
     @staticmethod
-    def ReadFile():
+    def read_file(indicate):
         try:
-            size = getsize(SHAXOR.inp)
+            size = path.getsize(SHAXOR.inp)
             with open(SHAXOR.inp, "rb") as f:
                 while True:
                     SHAXOR.time_temp = time()
@@ -132,31 +149,32 @@ class SHAXOR:
                         f.close()
                         break
                     yield chunk
-                    SHAXOR.UpdateProgress(f.tell(), size)
-        except IOError:
-            print("SHAXOR.py: error: opening input file occurred some error")
+                    if indicate:
+                        SHAXOR.update_progress(f.tell(), size)
+        except:
+            print("shaxor.py: error: opening input file occurred some error")
             exit()
 
     @staticmethod
-    def Factor(hs):
+    def factor(hs):
         sum = 0
         for i in range(0, 64, 8):
             sum += ord(hs[i])
         return sum
 
     @staticmethod
-    def EncFile():
+    def enc_file(indicate=True):
         try:
             o = open(SHAXOR.output, "wb+")
         except:
-            print("SHAXOR.py: error: opening output file occurred some error")
+            print("shaxor.py: error: opening output file occurred some error")
             exit()
         key = sha512(SHAXOR.key).digest()
-        for chunk in SHAXOR.ReadFile():
+        for chunk in SHAXOR.read_file(indicate=indicate):
             SHAXOR.key = ""
             for i in range(SHAXOR.chunk_size/64):
-                factor = SHAXOR.Factor(key)
-                hs = sha512(key *  (factor if factor > 0 else 1)).digest()
+                factor = SHAXOR.factor(key)
+                hs = sha512(key * (factor if factor > 0 else 1)).digest()
                 SHAXOR.key += hs
                 key = hs
             for i, c in enumerate(chunk):
@@ -164,34 +182,80 @@ class SHAXOR:
         o.close()
 
     @staticmethod
-    def EncText(decrypt=False):
+    def enc_text(decrypt=False, unit_test=False):
         if decrypt:
             try:
                 SHAXOR.inp = b64decode(SHAXOR.inp)
             except:
-                print("SHAXOR.py: error: decoding text occurred some error, encrypted text is probably damaged")
+                print("shaxor.py: error: decoding text occurred some error, encrypted text is probably damaged")
                 exit()
         XOR = ""
         SHAXOR.key = sha512(SHAXOR.key).digest()
-        factor = SHAXOR.Factor(SHAXOR.key)
+        factor = SHAXOR.factor(SHAXOR.key)
         SHAXOR.key = sha512(SHAXOR.key * (factor if factor > 0 else 1)).digest()
         for i, c in enumerate(SHAXOR.inp):
+            if i > 63:
+                i %= 64
             XOR += chr(ord(c) ^ ord(SHAXOR.key[i]))
             if i == (len(SHAXOR.key) - 1):
-                factor = SHAXOR.Factor(SHAXOR.key)
-                SHAXOR.key += sha512(SHAXOR.key * (factor if factor > 0 else 1)).digest()
+                factor = SHAXOR.factor(SHAXOR.key)
+                SHAXOR.key = sha512(SHAXOR.key * (factor if factor > 0 else 1)).digest()
         if not decrypt:
             XOR = b64encode(XOR)
-        if SHAXOR.output == None:
+        if unit_test:
+            return XOR
+        if SHAXOR.output is None:
             print(XOR)
         else:
             try:
                 open(SHAXOR.output, "r+").write(XOR)
             except:
-                print("SHAXOR.py: error: opening output file occurred some error")
+                print("shaxor.py: error: opening output file occurred some error")
 
+
+class UnitTest:
+    test_string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+
+    @staticmethod
+    def text_test():
+        SHAXOR.inp, SHAXOR.key = (UnitTest.test_string * 100), UnitTest.test_string
+        SHAXOR.inp = SHAXOR.enc_text(unit_test=True)
+        SHAXOR.key = UnitTest.test_string
+        if (UnitTest.test_string * 100) != SHAXOR.enc_text(unit_test=True, decrypt=True):
+            print("shaxor.py: error: Text encryption unit test error")
+            exit()
+        return True
+
+    @staticmethod
+    def file_test():
+        if path.isfile("unit_test.test"):
+            remove("unit_test.test")
+        if path.isfile("unit_test_enc.test"):
+            remove("unit_test_enc.test")
+        if path.isfile("unit_test_dec.test"):
+            remove("unit_test_dec.test")
+        open("unit_test.test", "w+").write(UnitTest.test_string * 100)
+        SHAXOR.key = UnitTest.test_string
+        SHAXOR.inp = "unit_test.test"
+        SHAXOR.output = "unit_test_enc.test"
+        SHAXOR.enc_file(indicate=False)
+        SHAXOR.key = UnitTest.test_string
+        SHAXOR.inp = "unit_test_enc.test"
+        SHAXOR.output = "unit_test_dec.test"
+        SHAXOR.enc_file(indicate=False)
+        res = open("unit_test_dec.test", "r").read()
+        remove("unit_test.test")
+        remove("unit_test_enc.test")
+        remove("unit_test_dec.test")
+        if res != (UnitTest.test_string * 100):
+            print("shaxor.py: error: File encryption unit test error!")
+            exit()
+        return True
 
 if __name__ == "__main__":
-    SHAXOR.SetArgs()
-    SHAXOR.ParseArgs()
-    SHAXOR.Decide()
+    SHAXOR.show_banner()
+    if UnitTest.text_test() and UnitTest.file_test():
+        print("        Unit tests was succesfull.\n")
+    SHAXOR.set_args()
+    SHAXOR.parse_args()
+    SHAXOR.decide()
